fe8e9431928f2ef92e258ca6b3f9e1c706b2453e
firminofl <30608181+firminofl@users.noreply.github.com>
2019-01-22 22:19:25 -0200

    Funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { AbsoluteDrag } from '../directives/absolute-drag/absolute-drag';
+import { AbsoluteDrag } from '../components/absolute-drag/absolute-drag';
 
 @NgModule({
   declarations: [
