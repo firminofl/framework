db79815ad575559dee2e9723832557240bdd6157
firminofl <30608181+firminofl@users.noreply.github.com>
2019-01-22 22:28:31 -0200

    Não serve directives.module.ts

--- src/directives/directives.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { AbsoluteDragDirective } from '../directives/absolute-drag/absolute-drag';
-@NgModule({
-	declarations: [AbsoluteDragDirective],
-	imports: [],
-	exports: [AbsoluteDragDirective]
-})
-export class DirectivesModule {}
